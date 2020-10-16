ae7fa88dddf65e7674f3e6191e4a2347356b319f
Josh Anderson <josh.anderson.1996@gmail.com>
2020-10-16 07:17:15 +0100

    inital commit

---
+++ resources/views/pages/content.blade.php
@@ -0,0 +1,146 @@
+@extends('partials.layout')
+
+@section('content')
+    @extends('partials.navbar')
+
+    
+    <div class="content-container">
+        
+        <div class="content-title">
+            <h1 class="main-heading">Content</h1>
+        </div>
+        
+
+        <div class="content-container__cards">
+        
+
+        <div class="content-cards">
+            <div class="content-cards__image">
+                <img class="cards__image" src="img/Crunchie-brownie.jpg" alt="">
+            </div>              
+            
+            <div class="content-cards-text">
+                <div class="content-cards__title">
+                    <p class="cards-title">Crunchie Brownie</p>
+                </div>
+
+                <div class="content-cards__prep">
+                    <p class="cards-prep">Prep <span class="cards-highlight">40 Mins.</span> </p>
+                </div>
+
+                <div class="content-cards__creator">
+                    <p class="cards-creator">By <span class="cards-highlight">Jordana Fulker</span> </p>
+                </div>
+                
+            </div>
+
+            <div class="content-cards__view">
+                <p class="cards-view">View</p>
+            </div>
+        </div>
+            
+
+        <div class="content-cards">
+            <div class="content-cards__image">
+                <img class="cards__image" src="img/Crunchie-brownie.jpg" alt="">
+            </div>              
+            
+            <div class="content-cards-text">
+                <div class="content-cards__title">
+                    <p class="cards-title">Crunchie Brownie</p>
+                </div>
+
+                <div class="content-cards__prep">
+                    <p class="cards-prep">Prep <span class="cards-highlight">40 Mins.</span> </p>
+                </div>
+
+                <div class="content-cards__creator">
+                    <p class="cards-creator">By <span class="cards-highlight">Jordana Fulker</span> </p>
+                </div>
+                
+            </div>
+            
+            <div class="content-cards__view">
+                <p class="cards-view">View</p>
+            </div>
+        </div>
+
+        <div class="content-cards">
+            <div class="content-cards__image">
+                <img class="cards__image" src="img/marble-millionair-shortbread.jpg" alt="">
+            </div>              
+            
+            <div class="content-cards-text">
+                <div class="content-cards__title">
+                    <p class="cards-title">Crunchie Brownie</p>
+                </div>
+
+                <div class="content-cards__prep">
+                    <p class="cards-prep">Prep <span class="cards-highlight">40 Mins.</span> </p>
+                </div>
+
+                <div class="content-cards__creator">
+                    <p class="cards-creator">By <span class="cards-highlight">Jordana Fulker</span> </p>
+                </div>
+                
+            </div>
+            
+            <div class="content-cards__view">
+                <p class="cards-view">View</p>
+            </div>
+        </div>
+
+        <div class="content-cards">
+            <div class="content-cards__image">
+                <img class="cards__image" src="img/Crunchie-brownie.jpg" alt="">
+            </div>              
+            
+            <div class="content-cards-text">
+                <div class="content-cards__title">
+                    <p class="cards-title">Crunchie Brownie</p>
+                </div>
+
+                <div class="content-cards__prep">
+                    <p class="cards-prep">Prep <span class="cards-highlight">40 Mins.</span> </p>
+                </div>
+
+                <div class="content-cards__creator">
+                    <p class="cards-creator">By <span class="cards-highlight">Jordana Fulker</span> </p>
+                </div>
+                
+            </div>
+            
+            <div class="content-cards__view">
+                <p class="cards-view">View</p>
+            </div>
+        </div>
+
+        <div class="content-cards">
+            <div class="content-cards__image">
+                <img class="cards__image" src="img/Crunchie-brownie.jpg" alt="">
+            </div>              
+            
+            <div class="content-cards-text">
+                <div class="content-cards__title">
+                    <p class="cards-title">Crunchie Brownie</p>
+                </div>
+
+                <div class="content-cards__prep">
+                    <p class="cards-prep">Prep <span class="cards-highlight">40 Mins.</span> </p>
+                </div>
+
+                <div class="content-cards__creator">
+                    <p class="cards-creator">By <span class="cards-highlight">Jordana Fulker</span> </p>
+                </div>
+                
+            </div>
+            
+            <div class="content-cards__view">
+                <p class="cards-view">View</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    
+
+@endsection
