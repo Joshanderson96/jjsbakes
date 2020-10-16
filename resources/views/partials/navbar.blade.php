5dac7b10b0d3cd4bbcfadce92bcc2885dda447b2
Josh Anderson <josh.anderson.1996@gmail.com>
2020-10-16 19:07:29 +0100

    Updated Navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,23 +1,26 @@
 <div class="navbar">
+   
+
     <div class="navbar-container">
-        <div class="navbar-menu-1 navbar-layout">
+
+            <div class="navbar-logo">
+                <img class="navbar-logo--image" src="img/jjsbakes-logo.png" alt="">
+            </div>
+            
+            
+        
             <ul class="navbar-options">
                 <a class="navbar-options__link" href="/"><li class="navbar-options-text">Home</li></a>
 
                 <a class="navbar-options__link" href="/content"><li class="navbar-options-text">Content</li></a>
-            </ul>
-        </div>
-
-        <div class="navbar-logo navbar-layout">
-            <img class="navbar-logo--image" src="img/jjsbakes-logo.png" alt="">
-        </div>
-        <div class="navbar-menu-2 navbar-layout">
-            <ul class="navbar-options">
                 <a class="navbar-options__link" href="/content"><li class="navbar-options-text">About</li></a>
 
                 <a class="navbar-options__link" href="/content"><li class="navbar-options-text">Socials</li></a>
             </ul>
-        </div>
+        
+
+       
+        
     </div>      
 </div>           
     
