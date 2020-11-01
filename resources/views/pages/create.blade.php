c3fc3f376d54c66a9a5e84f877ed0b29e4556c53
Josh Anderson <josh.anderson.1996@gmail.com>
2020-11-01 19:28:30 +0000

    styled input for create post form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,6 +4,17 @@
 
     @extends('partials.navbar')
 
+    
+    <div class="create-container">
+        <div class="create-title">
+            <h1 class="main-heading">Create Your Post</h1>
+        </div>
+
+        <form action="" class="create-form">
+            <label class="create-form__title" for="title">Title</label>
+            <input type="text" id="title" class="create-form__input">
+        </form>
+    </div>
 
     
 @endsection
