6fc047144193a4c74870fc8f4c67d0ce1ce9390a
Josh Anderson <josh.anderson.1996@gmail.com>
2020-11-03 11:59:24 +0000

    Created form selector and upload image button

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -13,6 +13,17 @@
         <form action="" class="create-form">
             <label class="create-form__title" for="title">Title</label>
             <input type="text" id="title" class="create-form__input">
+
+            <label class="create-form__title" for="category">Category</label>
+            <select name="category" class="create-form__select">
+                <option value="" disabled selected>Select your category</option>
+                <option value="Cake">Cake</option>
+                <option value="Brownies">Brownies</option>
+                <option value="Cookies">Cookies</option>
+            </select>
+
+            <label class="create-form__title" for="title">image</label>
+            <button class="crete-form__button">Upload an image</button>
         </form>
     </div>
 
